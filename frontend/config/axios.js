5f30ca1211aba77eb39ec0d619c7fc0348b1a43b
swasti <jainsanjna336@gmail.com>
2026-07-05 17:56:16 +0530

    url change

--- frontend/config/axios.js
+++ frontend/config/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getAccessToken, setAccessToken, clearAccessToken } from "../src/utils/token";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://styleora-mr50.onrender.com",
   withCredentials: true,
 });
 
